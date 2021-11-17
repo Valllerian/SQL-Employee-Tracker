f91b69c0cf7a4be5dcd9d674c9c6fd7efe98d436
Valerii Bihun <valeriibihun.co@gmail.com>
2021-11-16 22:25:07 -0700

    main inquirer prompt

--- index.js
+++ index.js
@@ -13,9 +13,30 @@ const db = mysql.createConnection(
       database: process.env.DB_NAME
     }
   );
-  
+
   db.connect( (err) => {
     if (err){
       throw error;
     }
   });
+
+
+// main inquirer function:
+
+function mainPrompt() {
+
+    return inquirer.prompt([
+      {
+          type: "list",
+          name: "displayAll",
+          message: "Hello! Please, select what you would to do.",
+          choices: ["View all departments", "View all roles", "View all employees", "Add department", "Add role", "Add employee", "Update Employee Role"]
+      }
+    ])
+   
+    .then((answers) => {
+
+     })
+};
+
+// write function for each selections;
