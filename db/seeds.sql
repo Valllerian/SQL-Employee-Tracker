209944b00e5554124eb1e43e242d01bcfe9a1d7c
Valerii Bihun <valeriibihun.co@gmail.com>
2021-11-16 19:33:30 -0700

    seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES (Marketing),
+(Operations),
+(Finance),
+(Sales),
+(HR),
+(Purchase);
+
+INSERT INTO role (id,title,salary,department_id)
+VALUES (001, "CEO","650000",2),
+(002, "Finance Director","350000",3),
+(003, "Program Director","350000",4),
+(004, "Operation Director","350000",2),
+(005, "Human Recources","250000",5),
+(006, "Marketing Director","350000",1), 
+(007, "Supply Director","350000",6);
